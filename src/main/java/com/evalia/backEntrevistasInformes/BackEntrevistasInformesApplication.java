fc5c4cff443f0680bce59277f76db27ff3c136a9
Angel <angelalvarezgon15@gmail.com>
2025-05-12 12:24:51 +0200

    primer proyecto

---
+++ src/main/java/com/evalia/backEntrevistasInformes/BackEntrevistasInformesApplication.java
@@ -0,0 +1,13 @@
+package com.evalia.backEntrevistasInformes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEntrevistasInformesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEntrevistasInformesApplication.class, args);
+	}
+
+}
