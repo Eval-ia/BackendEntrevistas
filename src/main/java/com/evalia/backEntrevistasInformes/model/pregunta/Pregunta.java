3dc7ad583b71f07b65362675e0b4d5b551a37c0b
Angel <angelalvarezgon15@gmail.com>
2025-05-13 09:24:26 +0200

    PreguntaVO

---
+++ src/main/java/com/evalia/backEntrevistasInformes/model/pregunta/Pregunta.java
@@ -0,0 +1,28 @@
+package com.evalia.backEntrevistasInformes.model.pregunta;
+
+import com.evalia.backEntrevistasInformes.model.puesto.PuestoTrabajoVO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "Pregunta")
+public class Pregunta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPregunta;
+
+    private String texto;
+
+    private Boolean esGenerica;
+
+    @ManyToOne
+    @JoinColumn(name = "id_puesto")
+    private PuestoTrabajoVO puesto;
+}
---
+++ src/main/java/com/evalia/backEntrevistasInformes/model/puesto/PuestoTrabajoVO.java
@@ -0,0 +1,32 @@
+package com.evalia.backEntrevistasInformes.model.puesto;
+
+import com.evalia.backEntrevistasInformes.model.categoria.CategoriaVO;
+import com.evalia.backEntrevistasInformes.model.nivel.NivelVO;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name ="Puesto")
+public class PuestoTrabajoVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idPuesto;
+
+    private String nombre;
+
+    private String descripcion;
+
+    @JoinColumn(name = "id_categoria")
+    private CategoriaVO categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_nivel")
+    private NivelVO nivel;
+}
