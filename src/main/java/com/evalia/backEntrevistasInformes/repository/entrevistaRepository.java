7000ae1632e2576310b456fb90d4ce42c1e7606b
alonso-teczz <diezgarciaalonso@gmail.com>
2025-05-14 09:47:38 +0200

    Cambios mínimos (para ver si se cambian los nombres de los repositorios)

--- src/main/java/com/evalia/backEntrevistasInformes/repository/entrevistaRepository.java
+++ src/main/java/com/evalia/backEntrevistasInformes/repository/entrevistaRepository.java
@@ -7,12 +7,14 @@ import org.springframework.data.repository.query.Param;
 import com.evalia.backEntrevistasInformes.model.entity.EntrevistaEntity;
 
 public interface EntrevistaRepository extends JpaRepository<EntrevistaEntity, Long> {
-    @Query("SELECT e FROM Entrevista e " +
-            "LEFT JOIN FETCH e.entrevistado " +
-            "LEFT JOIN FETCH e.entrevistador " +
-            "LEFT JOIN FETCH e.puesto " +
-            "LEFT JOIN FETCH e.preguntasPersonalizadas " +
-            "LEFT JOIN FETCH e.respuestas " +
-            "WHERE e.id = :id")
+    @Query(
+        "SELECT e FROM Entrevista e " +
+        "LEFT JOIN FETCH e.entrevistado " +
+        "LEFT JOIN FETCH e.entrevistador " +
+        "LEFT JOIN FETCH e.puesto " +
+        "LEFT JOIN FETCH e.preguntasPersonalizadas " +
+        "LEFT JOIN FETCH e.respuestas " +
+        "WHERE e.id = :id"
+    )
     EntrevistaEntity cargarEntrevistaCompleta(@Param("id") Long id);
 }
--- src/main/java/com/evalia/backEntrevistasInformes/service/ia/informe/IInformeEntrevistaService.java
+++ src/main/java/com/evalia/backEntrevistasInformes/service/ia/informe/IInformeEntrevistaService.java
@@ -1,8 +1,5 @@
 package com.evalia.backEntrevistasInformes.service.ia.informe;
 
-import com.evalia.backEntrevistasInformes.model.ia.recomendacion.ChatRequestDTO;
-import com.evalia.backEntrevistasInformes.model.ia.recomendacion.ChatResponseDTO;
-
 public interface IInformeEntrevistaService {
     void generarInformeDesdeEntrevista(Long idEntrevista);
 }
