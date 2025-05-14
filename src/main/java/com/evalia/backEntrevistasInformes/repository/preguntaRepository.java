e861eb1d2ecdde6ba606851f070af9fcf3f4cb3a
Angel <angelalvarezgon15@gmail.com>
2025-05-14 09:19:54 +0200

    PreguntaRepository

--- src/main/java/com/evalia/backEntrevistasInformes/repository/preguntaRepository.java
+++ src/main/java/com/evalia/backEntrevistasInformes/repository/preguntaRepository.java
@@ -1,9 +1,12 @@
 package com.evalia.backEntrevistasInformes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.evalia.backEntrevistasInformes.model.entity.PreguntaEntity;
 
 public interface preguntaRepository extends JpaRepository<PreguntaEntity, Long> {
 
+    List<PreguntaEntity> findByPuesto_CategoriaAndPuesto_NivelAndEsGenerica(String categoria, String nivel, Boolean esGenerica);
 }
