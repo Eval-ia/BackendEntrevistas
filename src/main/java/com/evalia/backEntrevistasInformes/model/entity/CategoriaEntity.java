44ef09a85126344822578ec689d939510a9c0757
Angel <angelalvarezgon15@gmail.com>
2025-05-15 12:31:15 +0200

    Borrado

--- src/main/java/com/evalia/backEntrevistasInformes/model/entity/CategoriaEntity.java
+++ src/main/java/com/evalia/backEntrevistasInformes/model/entity/CategoriaEntity.java
@@ -1,15 +1,11 @@
 package com.evalia.backEntrevistasInformes.model.entity;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
--- src/main/java/com/evalia/backEntrevistasInformes/repository/preguntaRepository.java
+++ src/main/java/com/evalia/backEntrevistasInformes/repository/preguntaRepository.java
@@ -1,11 +1,8 @@
 package com.evalia.backEntrevistasInformes.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.evalia.backEntrevistasInformes.model.entity.CategoriaEntity;
-import com.evalia.backEntrevistasInformes.model.entity.NivelEntity;
 import com.evalia.backEntrevistasInformes.model.entity.PreguntaEntity;
 
 public interface preguntaRepository extends JpaRepository<PreguntaEntity, Long> {
