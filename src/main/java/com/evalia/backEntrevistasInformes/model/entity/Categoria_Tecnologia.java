0ff8e3fe89c4a3bdef3ca28fd2fcc2b6be2c5ca8
Angel <angelalvarezgon15@gmail.com>
2025-05-15 12:45:55 +0200

    Borrado de Entity Tecnologia y Añadido el Enlace

--- src/main/java/com/evalia/backEntrevistasInformes/model/entity/Categoria_Tecnologia.java
+++ src/main/java/com/evalia/backEntrevistasInformes/model/entity/Categoria_Tecnologia.java
@@ -1,13 +1,17 @@
 package com.evalia.backEntrevistasInformes.model.entity;
 
+import java.util.List;
+
 import com.evalia.backEntrevistasInformes.model.NombreCategoriaTecnologia;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -20,4 +24,8 @@ public class Categoria_Tecnologia {
 
     @Enumerated(EnumType.STRING)
     private NombreCategoriaTecnologia nombre;
+
+
+    @OneToMany(mappedBy = "categoriaTecnologia", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CategoriaEntity> categorias;
 }
