453650ad043034073309f513f6da793a8a462d9f
Angel <angelalvarezgon15@gmail.com>
2025-05-19 09:41:29 +0200

    Commit

--- src/main/java/com/evalia/backEntrevistasInformes/service/survey/IPreguntaService.java
+++ src/main/java/com/evalia/backEntrevistasInformes/service/survey/IPreguntaService.java
@@ -1,12 +1,15 @@
 package com.evalia.backEntrevistasInformes.service.survey;
 
-import com.evalia.backEntrevistasInformes.model.entity.PreguntaEntity;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import com.evalia.backEntrevistasInformes.model.entity.PreguntaEntity;
 
 public interface IPreguntaService {
     List<PreguntaEntity> findAll();
     Optional<PreguntaEntity> findById(Long id);
     PreguntaEntity save(PreguntaEntity pregunta);
     void deleteById(Long id);
+    Map<String, List<String>> obtenerPreguntasPorCategoriaYNivel(String categoria, String nivel);
 }
